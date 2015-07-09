ec44c43126ef0cb06441a5b396224e72c27f1909
Landon Alder <landon.d.alder@exchange.nordstrom.com>
2015-07-09 14:46:48 -0700

    Add user status text to mood table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130000519) do
+ActiveRecord::Schema.define(version: 20150708193928) do
 
   create_table "moods", force: true do |t|
     t.integer  "top"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150130000519) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   create_table "users", force: true do |t|
